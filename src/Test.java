6985fe80422c269309557ff4f32ae7675d52fa8e
is247 <32758777+is247@users.noreply.github.com>
2017-11-15 19:23:19 -0500

    Update Test.java

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
+
 public class Test {
     public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        
         System.out.println("Hello");
-        System.out.println("Are you there");
     }
 }
